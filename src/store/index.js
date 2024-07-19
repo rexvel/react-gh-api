9b6c65d11cb7e2fcd3ff3d7fd1253206ed11b5d5
rexvel <rexvelele@gmail.com>
2024-07-19 13:55:25 +0300

    store: update config

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,11 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunkMiddleware from "redux-thunk";
-import rootReducer from "./reducers/index";
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-      applyMiddleware(thunkMiddleware)
-  ),
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
