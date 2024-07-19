feee7cbe7346de69e9fac083e6e0bb426407327d
rexvel <rexvelele@gmail.com>
2024-07-19 14:16:02 +0300

    fix: update for react 18 support

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import AppRoot from './view/components/AppRoot';
 import * as serviceWorker from './serviceWorker';
 
-// eslint-disable-next-line import/prefer-default-export
-export const history = createBrowserHistory();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <AppRoot />
-    </Router>
-  </Provider>,
-
-  document.getElementById('root'),
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRoot />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
