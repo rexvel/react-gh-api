ae505d76a89512101043d2180ad593ea65afb1e7
rexvel <rexvelele@gmail.com>
2024-07-19 14:16:54 +0300

    fix: minor updates

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers/index';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 const store = configureStore({
   reducer: rootReducer,
